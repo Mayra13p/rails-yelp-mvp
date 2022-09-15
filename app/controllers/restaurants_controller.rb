2e75320ece18747a83a301cc2e30c992afe76339
Mayra Moreno <malejandramp13@gmail.com>
2022-09-15 18:44:48 -0500

    backend ready

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,11 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: %i[show edit update destroy]
 
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
@@ -14,22 +14,24 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-    redirect_to @restaurant
+
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render :new
+    end
   end
 
   def edit
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.update(restaurant_params)
     redirect_to @restaurant
   end
 
   def destroy
-    @restaurant = Restaurant.find(params[:id])
+    # @restaurant.reviews.clear
     @restaurant.destroy
     redirect_to restaurants_path, status: :see_other
   end
@@ -38,5 +40,9 @@ class RestaurantsController < ApplicationController
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
